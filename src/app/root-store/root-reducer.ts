632307be84a7c1afa897d8d84fae6d41b88a433c
aafathi <aliasghar.fathi@altran.it>
2020-12-10 16:47:30 +0100

    modifications

--- src/app/root-store/root-reducer.ts
+++ src/app/root-store/root-reducer.ts
@@ -1,11 +1,20 @@
-import {ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
 import {State} from '@root-store/state';
 import {environment} from '../../environments/environment';
+import {localStorageSync} from 'ngrx-store-localstorage';
 
 
+// tslint:disable-next-line:no-empty-interface
 export interface AppState {
 }
 
 export const reducers: ActionReducerMap<AppState> = {};
+//
+// export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+// const reducers: ActionReducerMap<State> = {todos, visibilityFilter};
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({rehydrate: true, keys: ['auth']})(reducer);
+}
+export const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
